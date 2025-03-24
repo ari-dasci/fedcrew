630f836aaf2925174c13b8c1092ad516360078db
mariogmarq <mariogarciamarq@gmail.com>
2025-03-24 09:25:35 +0100

    Refactor models.py into using dataclass config

--- models.py
+++ models.py
@@ -1,102 +1,78 @@
+from dataclasses import dataclass
+from typing import Callable
+
+import torch
 from torch import nn
+from torch.nn import functional as F
 from torchvision import transforms
 from torchvision.models import resnet18, ResNet18_Weights
-from torch.nn import functional as F
-import torch
 
-def get_model(dataset: str):
-    match dataset:
-        case "celeba" | "waterbirds":
-            return _get_resnet(num_classes=2)
-            # return CelebaNet(num_classes=2)
-        case _:
-            return _get_resnet(num_classes=10)
+
+@dataclass
+class DatasetConfig:
+    num_classes: int
+    transforms: Callable[[], transforms.Compose]
+    relevance_layer: str
+    model_factory: Callable[[], nn.Module]
+
 
 def _get_resnet(num_classes: int = 10, pretraining=False):
     model = resnet18()
     model.fc = nn.Linear(model.fc.in_features, num_classes, bias=False)
     return model
 
-def get_relevance_layer(dataset: str):
-    match dataset:
-        # case "celeba":
-        #     return "conv4"
-        case _:
-            return "layer4.1.conv2"
+
+DATASET_CONFIG = {"celeba": DatasetConfig(num_classes=2, transforms=lambda: transforms.Compose(
+    [transforms.ToPILImage(), ResNet18_Weights.DEFAULT.transforms()]), relevance_layer="conv4",
+                                          model_factory=lambda: _get_resnet(num_classes=2)),
+                  "waterbirds": DatasetConfig(num_classes=2, transforms=lambda: ResNet18_Weights.DEFAULT.transforms(),
+                                              relevance_layer="layer4.1.conv2",
+                                              model_factory=lambda: _get_resnet(num_classes=2)),
+                  "default": DatasetConfig(num_classes=10, transforms=lambda: ResNet18_Weights.DEFAULT.transforms(),
+                                           relevance_layer="layer4.1.conv2",
+                                           model_factory=lambda: _get_resnet(num_classes=10))}
+
+
+def get_model(dataset: str):
+    config = DATASET_CONFIG.get(dataset, DATASET_CONFIG["default"])
+    return config.model_factory()
+
 
 def get_transforms(dataset: str):
-    match dataset:
-        case "celeba":
-            from torchvision.models import ResNet18_Weights
-            # return transforms.Compose(
-            #     [
-            #         transforms.ToPILImage(),
-            #         transforms.Resize((224, 224)),
-            #         transforms.Grayscale(),
-            #         transforms.ToTensor(),
-            #     ]
-            # )
-            return transforms.Compose([transforms.ToPILImage(), ResNet18_Weights.DEFAULT.transforms()])
-        case _:
-            from torchvision.models import ResNet18_Weights
-            return ResNet18_Weights.DEFAULT.transforms()
+    config = DATASET_CONFIG.get(dataset, DATASET_CONFIG["default"])
+    return config.transforms()
+
+
+def get_relevance_layer(dataset: str):
+    config = DATASET_CONFIG.get(dataset, DATASET_CONFIG["default"])
+    return config.relevance_layer
 
 
 class CelebaNet(nn.Module):
     def __init__(self, num_classes):
         super().__init__()
-        self.conv1 = nn.Conv2d(
-            in_channels=1, out_channels=32, kernel_size=3, padding="same"
-        )
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding="same")
         self.bn1 = nn.BatchNorm2d(32)
         self.drop1 = nn.Dropout(p=0.4)
-
-        self.conv2 = nn.Conv2d(
-            in_channels=32, out_channels=32, kernel_size=3, padding="same"
-        )
+        self.conv2 = nn.Conv2d(32, 32, kernel_size=3, padding="same")
         self.bn2 = nn.BatchNorm2d(32)
         self.drop2 = nn.Dropout(p=0.4)
-
-        self.conv3 = nn.Conv2d(
-            in_channels=32, out_channels=32, kernel_size=3, padding="same"
-        )
+        self.conv3 = nn.Conv2d(32, 32, kernel_size=3, padding="same")
         self.bn3 = nn.BatchNorm2d(32)
         self.drop3 = nn.Dropout(p=0.4)
-
-        self.conv4 = nn.Conv2d(
-            in_channels=32, out_channels=32, kernel_size=3, padding="same"
-        )
+        self.conv4 = nn.Conv2d(32, 32, kernel_size=3, padding="same")
         self.bn4 = nn.BatchNorm2d(32)
         self.drop4 = nn.Dropout(p=0.4)
-
         self.fc = nn.Linear(32 * 14 * 14, num_classes)
 
     def forward(self, x):
-        # One block
-        x = self.conv1(x)
-        x = self.bn1(x)
+        x = self.drop1(F.relu(self.bn1(self.conv1(x))))
         x = F.max_pool2d(x, 2, 2)
-        x = self.drop1(F.relu(x))
-        # print("First block", x.shape) # (None, 112, 112, 32)
-
-        x = self.conv2(x)
-        x = self.bn2(x)
+        x = self.drop2(F.relu(self.bn2(self.conv2(x))))
         x = F.max_pool2d(x, 2, 2)
-        x = self.drop2(F.relu(x))
-        # print("Second block", x.shape) # (None, 56, 56, 32)
-
-        x = self.conv3(x)
-        x = self.bn3(x)
+        x = self.drop3(F.relu(self.bn3(self.conv3(x))))
         x = F.max_pool2d(x, 2, 2)
-        x = self.drop3(F.relu(x))
-        # print("Third block", x.shape) # (None, 28, 28, 32)
-
-        x = self.conv4(x)
-        x = self.bn4(x)
+        x = self.drop4(F.relu(self.bn4(self.conv4(x))))
         x = F.max_pool2d(x, 2, 2)
-        x = self.drop4(F.relu(x))
-        # print("Fourth block", x.shape) # (None, 14, 14, 32)
         x = torch.flatten(x, 1)
-        x = self.fc(x)
-
-        return x  # self.ac(x)
+        return self.fc(x)
